9f1cb198078babc209581b8797a120b8a7d35439
DavisButler10 <davis.d.butler@gmail.com>
2020-01-22 00:39:54 -0700

    Spawning

---
+++ Assets/Scripts/KinematicRot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicRot : MonoBehaviour
+{
+    float maxSpeed = 5f; //speed of enemy
+    public Transform player; //target
+    Seek mySeek = new Seek();
+
+    public Vector3 linearVelocity;
+    public float angularVelocity;
+ 
+    void Update()
+    {
+        transform.position += linearVelocity * Time.deltaTime;
+        transform.eulerAngles += new Vector3(0, angularVelocity * Time.deltaTime, 0);
+
+        KinematicSteeringOutput steering = mySeek.getSteering();
+
+
+    }
+
+}
